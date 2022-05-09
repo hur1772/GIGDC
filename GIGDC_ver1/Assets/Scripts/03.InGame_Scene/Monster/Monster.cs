d8fd7d65586b5e689dcbea79c6137b99b2838e16
csm9278 <csm9278@gmail.com>
2022-05-09 19:18:44 +0900

    Pig Ai_ver1
    Move, Patrol, Chase, Attack

--- GIGDC_ver1/Assets/Scripts/03.InGame_Scene/Monster/Monster.cs
+++ GIGDC_ver1/Assets/Scripts/03.InGame_Scene/Monster/Monster.cs
@@ -2,17 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MonsterState
+{
+    IDLE,
+    PATROL,
+    CHASE,
+    ATTACK,
+    DIE
+}
+
 public class Monster : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //플레이어, 거리 구하기용 벡터
+    protected GameObject m_Player;
+    protected Vector2 m_CalcVec = Vector3.zero;
+    protected Rigidbody2D m_Rb;
+
+    //몬스터 기본 스탯, 상태
+    protected float m_MaxHP = 0;
+    protected float m_CurHP = 0;
+    protected float m_Atk   = 0;
+    protected float m_MoveSpeed = 0;
+
+    protected float m_ChaseDistance = 0;
+    protected float m_AttackDistance = 0;
+
+    public MonsterState m_Monstate = MonsterState.IDLE;
+    protected Animator m_Animator;
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
         
     }
+
+    protected virtual void TakeDamage(float a_Value)
+    {
+        m_CurHP -= a_Value;
+        if (m_CurHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    protected virtual void Die()
+    {
+        m_CurHP = 0;
+        Debug.Log("Die");
+    }
+
 }
