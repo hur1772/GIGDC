b2005f969c2b0eb9addae4e5521441b13ebd3d21
Chan1605 <qkqhoe@naver.com>
2022-06-08 22:36:57 +0900

    hpbar

--- Assets/Scripts/UI/UIMgr.cs
+++ Assets/Scripts/UI/UIMgr.cs
@@ -8,11 +8,13 @@ public class UIMgr : MonoBehaviour
     public GameObject ShopPanel;
 
     public Button CloseBtn = null;
-
+    public Image m_HpBar = null;
     public Image UseItemImg1 = null;
     public Image UseItemImg2 = null;
     public Image UseItemImg3 = null;
 
+    float m_CurHp;
+    float m_MaxHp;
     float UseItemCoolTime1 = 5.0f;
     float UseItemCoolTime2 = 5.0f;
     float UseItemCoolTime3 = 5.0f;
@@ -20,72 +22,96 @@ public class UIMgr : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (CloseBtn != null)
-        {
-            CloseBtn.onClick.AddListener(CloseBtnFunc);
-        }
+        m_CurHp = 100.0f;
+        m_MaxHp = m_CurHp;
+        //if (CloseBtn != null)
+        //{
+        //    CloseBtn.onClick.AddListener(CloseBtnFunc);
+        //}
+        Time.timeScale = 1.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (ShopPanel != null)
-        {
-            if (ShopPanel.activeSelf == true)
-            {
-                if (Input.GetKey(KeyCode.Escape))
-                {
-                    ShopPanel.SetActive(false);
-                }
-            }
-        }
+        TakeDamage(0.01f);
+        //if (ShopPanel != null)
+        //{
+        //    if (ShopPanel.activeSelf == true)
+        //    {
+        //        if (Input.GetKey(KeyCode.Escape))
+        //        {
+        //            ShopPanel.SetActive(false);
+        //        }
+        //    }
+        //}
 
-        if (Input.GetKey(KeyCode.Alpha1) && UseItemCoolTime1 >= 5.0f)
-        {
-            if (UseItemImg1 != null)
-            {
-                UseItemCoolTime1 = 0.0f;
-            }
-        }
+        //if (Input.GetKey(KeyCode.Alpha1) && UseItemCoolTime1 >= 5.0f)
+        //{
+        //    if (UseItemImg1 != null)
+        //    {
+        //        UseItemCoolTime1 = 0.0f;
+        //    }
+        //}
 
-        if (Input.GetKey(KeyCode.Alpha2) && UseItemCoolTime2 >= 5.0f)
-        {
-            if (UseItemImg2 != null)
-            {
-                UseItemCoolTime2 = 0.0f;
-            }
-        }
+        //if (Input.GetKey(KeyCode.Alpha2) && UseItemCoolTime2 >= 5.0f)
+        //{
+        //    if (UseItemImg2 != null)
+        //    {
+        //        UseItemCoolTime2 = 0.0f;
+        //    }
+        //}
 
-        if (Input.GetKey(KeyCode.Alpha3) && UseItemCoolTime3 >= 5.0f)
-        {
-            if (UseItemImg3 != null)
-            {
-                UseItemCoolTime3 = 0.0f;
-            }
-        }
+        //if (Input.GetKey(KeyCode.Alpha3) && UseItemCoolTime3 >= 5.0f)
+        //{
+        //    if (UseItemImg3 != null)
+        //    {
+        //        UseItemCoolTime3 = 0.0f;
+        //    }
+        //}
 
-        if (UseItemCoolTime1 <= 5.0f)
-        {
-            UseItemCoolTime1 += Time.deltaTime;
-            UseItemImg1.fillAmount = UseItemCoolTime1 / 5;
-        }
+        //if (UseItemCoolTime1 <= 5.0f)
+        //{
+        //    UseItemCoolTime1 += Time.deltaTime;
+        //    UseItemImg1.fillAmount = UseItemCoolTime1 / 5;
+        //}
 
-        if (UseItemCoolTime2 <= 5.0f)
-        {
-            UseItemCoolTime2 += Time.deltaTime;
-            UseItemImg2.fillAmount = UseItemCoolTime2 / 5;
-        }
+        //if (UseItemCoolTime2 <= 5.0f)
+        //{
+        //    UseItemCoolTime2 += Time.deltaTime;
+        //    UseItemImg2.fillAmount = UseItemCoolTime2 / 5;
+        //}
 
-        if (UseItemCoolTime3 <= 5.0f)
-        {
-            UseItemCoolTime3 += Time.deltaTime;
-            UseItemImg3.fillAmount = UseItemCoolTime3 / 5;
-        }
+        //if (UseItemCoolTime3 <= 5.0f)
+        //{
+        //    UseItemCoolTime3 += Time.deltaTime;
+        //    UseItemImg3.fillAmount = UseItemCoolTime3 / 5;
+        //}
     }
 
     void CloseBtnFunc()
     {
         ShopPanel.SetActive(false);
         Time.timeScale = 1.0f;
+    }
+
+    public void TakeDamage(float a_val)
+    {
+        if (m_CurHp <= 0.0f)
+            return;
+
+        m_CurHp -= a_val;
+
+        if (m_HpBar != null)
+            m_HpBar.fillAmount = m_CurHp / m_MaxHp;
+
+        if(m_CurHp <=0.0f)
+        {
+            Debug.Log("»ç¸Á!!");
+            // PlayerDie();
+
+            Time.timeScale = 0.0f;
+        }
+        
     }
 }
