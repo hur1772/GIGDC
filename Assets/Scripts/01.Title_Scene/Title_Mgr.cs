e81d82971a477b511badd0be6d89a11a7b2ad446
ChangDae1993 <jsj939639@gmail.com>
2022-07-01 22:55:24 +0900

    Scene 연출 추가 中

--- Assets/Scripts/01.Title_Scene/Title_Mgr.cs
+++ Assets/Scripts/01.Title_Scene/Title_Mgr.cs
@@ -6,34 +6,21 @@ using UnityEngine.SceneManagement;
 
 public class Title_Mgr : MonoBehaviour
 {
-    public Button StartBtn;
-    public Button CreditBtn;
 
     private void Start() => StartFunc();
 
     private void StartFunc()
     {
-        if (StartBtn != null)
-            StartBtn.onClick.AddListener(StartBtnFunc);
 
-        if (CreditBtn != null)
-            CreditBtn.onClick.AddListener(CreditSceneFunc);
     }
 
     private void Update() => UpdateFunc();
 
     private void UpdateFunc()
     {
-        
-    }
-
-    void StartBtnFunc()
-    {
-        SceneManager.LoadScene("01.Stage_1");
-    }
-
-    void CreditSceneFunc()
-    {
-
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("01.Stage_1");
+        }
     }
 }
