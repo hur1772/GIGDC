7df7dc1711b33dc795190867cc9762f3d463f23c
hur1772 <49256922+hur1772@users.noreply.github.com>
2022-07-02 00:55:56 +0900

    막기

--- Assets/Scripts/03.InGame_Scene/Player/PlayerLimit/VillageLimit.cs
+++ Assets/Scripts/03.InGame_Scene/Player/PlayerLimit/VillageLimit.cs
@@ -1,30 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class VillageLimit : MonoBehaviour
 {
+    Scene nowscene;
+
     private Transform tr;
     private Vector2 m_Pos = Vector2.zero;
     // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
         tr = GetComponent<Transform>();
+    }
+
+    void OnEnable()
+    {
+        // 씬 매니저의 sceneLoaded에 체인을 건다.
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    // 체인을 걸어서 이 함수는 매 씬마다 호출된다.
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        nowscene = SceneManager.GetActiveScene();
+        Debug.Log(nowscene);
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_Pos = tr.position;
-        if (tr.position.x < -7.0f)
+        if (nowscene.name == "Village")
         {
-            m_Pos.x = -7.0f;
-        }
-        if (86.0f < tr.position.x)
-        {
-            m_Pos.x = 86.0f;
-        }
+            m_Pos = tr.position;
+            if (tr.position.x < -7.0f)
+            {
+                m_Pos.x = -7.0f;
+            }
+            if (86.0f < tr.position.x)
+            {
+                m_Pos.x = 86.0f;
+            }
 
-        tr.position = m_Pos;
+            tr.position = m_Pos;
+        }
+        
     }
 }
