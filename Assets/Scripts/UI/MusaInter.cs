fbc200dc0b58dcd42b3a128769d43f838350170c
Chan1605 <qkqhoe@naver.com>
2022-07-05 23:46:28 +0900

    추추추추추추가가가가

---
+++ Assets/Scripts/UI/MusaInter.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusaInter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Interaction.Inst.UpgdNPCDistance = Vector2.Distance(Interaction.Inst.PlayPos.transform.position, this.transform.position);
+        MusaNPCInter();
+    }
+
+    void MusaNPCInter()
+    {
+        //if (Interaction.Inst.IsInteraction == false)
+        //{
+        //    if (Interaction.Inst.UpgdNPCDistance < Interaction.Inst.PortalDistance || Interaction.Inst.UpgdNPCDistance < Interaction.Inst.ShopDistance || Interaction.Inst.UpgdNPCDistance < Interaction.Inst.HealItemShopDistance || Interaction.Inst.UpgdNPCDistance < Interaction.Inst.BeaconDistance)
+        //    {
+        //        if (Interaction.Inst.UpgdNPCDistance < 5.0f)
+        //        {
+        //            if (Interaction.Inst.GKey != null)
+        //            {
+        //                InfoUI.Inst.WPStore.gameObject.SetActive(true);
+        //                Interaction.Inst.GKey.gameObject.SetActive(true);
+        //                Interaction.Inst.animator.SetFloat("Interaction", Interaction.Inst.UpgdNPCDistance);
+        //                Interaction.Inst.m_interactionState = InteractionState.UpGdNPC;
+        //                Interaction.Inst.IsInteraction = true;
+        //            }
+        //        }
+        //    }
+        //}
+        //if (Interaction.Inst.IsInteraction == true)
+        //{
+        //    if (Interaction.Inst.HealItemShopDistance > 5.0f && Interaction.Inst.ShopDistance > 5.0f && Interaction.Inst.PortalDistance > 5.0f && Interaction.Inst.BeaconDistance > 5.0f)
+        //    {
+        //        if (Interaction.Inst.UpgdNPCDistance > 5.0f)
+        //        {
+        //            if (Interaction.Inst.GKey != null)
+        //            {
+        //                InfoUI.Inst.WPStore.gameObject.SetActive(false);
+        //                Interaction.Inst.GKey.gameObject.SetActive(false);
+        //                Interaction.Inst.m_interactionState = InteractionState.Nomal;
+        //                Interaction.Inst.animator.SetFloat("Interaction", Interaction.Inst.UpgdNPCDistance);
+        //                Interaction.Inst.IsInteraction = false;
+        //            }
+        //        }
+        //    }
+        //}
+    }
+}
